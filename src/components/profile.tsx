386f87542061ef73320684db9ea2ef938b05d9fb
evening8 <ishimwerichard26@gmail.com>
2023-10-29 23:51:52 +0200

    case study ui

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -14,7 +14,7 @@ const Profile = () => {
           <p className='text-gray-500 font-medium'>product engineer</p>
         </div>
         <button className='px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-600 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'>
-          Message
+          Message.
         </button>
       </div>
     </div>
